8be3b9904f4101a158e96297d3775b37963c849f
Fazrin <nirzaf@gmail.com>
2019-09-28 16:31:00 +0530

    db connection update

--- database/db.php
+++ database/db.php
@@ -1,5 +1,5 @@
 <?php
-	$database_username = 'root';//user name
-	$database_password = '';//password
-	$pdo_conn = new PDO( 'mysql:host=localhost;dbname=blog_admin_db', $database_username, $database_password );
+	$database_username = 'unaux_24548161';//user name
+	$database_password = '42wr4ts2w3c5';//password
+	$pdo_conn = new PDO( 'mysql:host=sql200.unaux.com;dbname=unaux_24548161_demoacc', $database_username, $database_password );
 ?>
